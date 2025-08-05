5bc6f3cbc31a20af7eb816a18b02b0a68ba05f73
adriancpp <adrian.cpp@wp.pl>
2025-08-05 09:59:34 +0200

    persistance layout

--- resources/js/Pages/Index/Show.vue
+++ resources/js/Pages/Index/Show.vue
@@ -1,11 +1,15 @@
 <template>
-    <MainLayout>
-        <div>Show</div>
-        <Link href="/">IndexPage</Link>
-    </MainLayout>
+    <div>Show</div>
+    <Link href="/">IndexPage</Link>
 </template>
 
 <script setup>
-    import {Link} from '@inertiajs/vue3'
+import {Link} from '@inertiajs/vue3'
+</script>
+
+<script>
 import MainLayout from '../../Layouts/MainLayout.vue';
+export default {
+    layout: MainLayout
+}
 </script>
