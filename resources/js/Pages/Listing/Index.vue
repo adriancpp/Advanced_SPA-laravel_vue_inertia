97ab51d30c73d8305cf39633f72661c38def3e8e
adriancpp <adrian.cpp@wp.pl>
2025-08-19 19:28:56 +0200

    custom ui and grid responsive

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,21 +1,24 @@
 <template>
-  <div v-for="listing in listings" :key="listing.id">
-    <div>
-      <Link :href="route('listing.show', {listing: listing.id})">
-        <ListingAddress :listing="listing" />
-      </Link>
-    </div>
-    <div>
-      <Link :href="route('listing.edit', {listing: listing.id})" as="button">Edit</Link>
-    </div>
-    <div>
-      <Link :href="route('listing.destroy', {listing: listing.id})" method="delete" as="button">Delete</Link>
-    </div>
+  <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+    <Box v-for="listing in listings" :key="listing.id">
+      <div>
+        <Link :href="route('listing.show', {listing: listing.id})">
+          <ListingAddress :listing="listing" />
+        </Link>
+      </div>
+      <div>
+        <Link :href="route('listing.edit', {listing: listing.id})" as="button">Edit</Link>
+      </div>
+      <div>
+        <Link :href="route('listing.destroy', {listing: listing.id})" method="delete" as="button">Delete</Link>
+      </div>
+    </Box>
   </div>
 </template>
 
 <script setup>
 import ListingAddress from '@/Components/ListingAddress.vue'
+import Box from '@/Components/UI/Box.vue'
 import { Link } from '@inertiajs/vue3'
 defineProps({
   listings: Array,
